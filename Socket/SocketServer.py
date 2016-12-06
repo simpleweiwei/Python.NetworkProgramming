240230fb52b4b3d22e33665d0fd95cc45c3623f5
simpleweiwei <coderweiwei@outlook.com>
2016-12-06 15:38:20 +0800

    Update SocketServer.py

--- Socket/SocketServer.py
+++ Socket/SocketServer.py
@@ -14,11 +14,13 @@ def socket_server(port):
     s.bind(server_address)
     s.listen(backlog)
     while True:
-        client, address = s.accept()
+        client, address = s.accept() 
+        # 接受连接并返回(client, address),其中client是新的套接字对象,用来接受同时也可以发送数据,address是连接客户端的地址,此时是阻塞状态
+        # 注意进行数据交换的是新的套接字对象client,并不是刚开始创建的套接字对象
         data = client.recv(data_playload)
         if data:
-            print("data:", data)
-            client.send(data)
+            print("data:", data)  # data可以通过data.decode()进行解码
+            client.send(data)  # 可以通过client.sendall(bytes('XXXXX',encoding='utf8')) 发送数据,注意python3中发送必须是bytes格式的
             print("sent {} bytes back to {}".format(data, address))
         client.close()
 
